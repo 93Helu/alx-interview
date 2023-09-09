8c495b31b5f887f651ee6e1baad52992c052f234
Helen <114483343+93Helu@users.noreply.github.com>
2023-09-09 10:33:17 -0700

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,43 +1,36 @@
 #!/usr/bin/node
 
+const mainUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 const request = require('request');
-const movieID = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieID}/`;
 
-async function main () {
-  try {
-    request(url, async (error, response, body) => {
-      if (error) {
-        console.log(error);
-        return;
-      }
-      if (response.statusCode === 200) {
-        const film = JSON.parse(body);
-        const characters = film.characters;
-        for (const characterUrl of characters) {
-          try {
-            const characterData = await new Promise((resolve, reject) => {
-              request(characterUrl, (error, response, characterBody) => {
-                if (error) {
-                  reject(error);
-                } else {
-                  const character = JSON.parse(characterBody);
-                  resolve(character.name);
-                }
-              });
-            });
-            console.log(characterData);
-          } catch (characterError) {
-            console.error('Character Error:', characterError);
-          }
-        }
+// Main Request
+request(mainUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const urlArray = JSON.parse(body).characters;
+    myAsyn(urlArray);
+  }
+});
+
+// Sub request function
+function subRequest (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (error2, response2, body2) {
+      if (error2) {
+        reject(error2);
       } else {
-        console.log('Error code: ' + response.statusCode);
+        resolve(JSON.parse(body2).name);
       }
     });
-  } catch (error) {
-    console.error('Error:', error);
-  }
+  });
 }
 
-main();
+// Asynchronous function awaits for
+// sub request resolution after each iterations
+async function myAsyn (urlArray) {
+  for (const url of urlArray) {
+    const character = await subRequest(url);
+    console.log(character);
+  }
+}
